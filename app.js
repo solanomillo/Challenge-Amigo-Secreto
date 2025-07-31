ad024385bf3b9889cefc1445cb0af58156b0da6f
solanomillo <solanomillo144@gmail.con>
2025-07-31 15:37:51 -0300

    se agrego la funcion agregarAmigo-limpiarCaja

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function agregarAmigo(){
+    let datoUsuario = document.getElementById('amigo').value;
+    if (datoUsuario != ''){
+        nombresAmigos.push(datoUsuario);
+        limpiarCajar();
+    }
+    else{
+        alert('Por favor, inserte un nombre');
+    }
+}
+
+function limpiarCajar(){
+    document.getElementById('amigo').value = ''
+}
